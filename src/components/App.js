4bdc5727014be9e06bad5aeeb26043f0d78964f8
+ <+>
2023-01-19 13:38:07 +0300

    Made changes to file

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import {Routes, Route } from "react-router-dom";
+import {Switch, Route } from "react-router-dom";
 import NavBar from "./NavBar";
 import MoviesPage from "../components/MoviesPage";
 
@@ -14,14 +14,14 @@ function App() {
   return (
     <div>
       <NavBar />
-      <Routes>
+      <Switch>
         <Route path="/movies">
           <MoviesPage movies={movies} />
         </Route>
         <Route exact path="/">
           <div>Home</div>
         </Route>
-      </Routes>
+      </Switch>
     </div>
   );
 }
